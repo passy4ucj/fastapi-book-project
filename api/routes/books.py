e312feaf0bfccd62b77247edd8415d4693bd9620
passy4ucj <ojinnnakapascal@gmail.com>
2025-02-13 16:56:06 +0100

    fix docker deploy

--- api/routes/books.py
+++ api/routes/books.py
@@ -49,6 +49,7 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
+
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book(book_id: int) -> Book:
     book = db.books.get(book_id)
